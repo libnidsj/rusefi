99a4253d218f91e652b5b92296aca4b38e6ca064
Líbni Junior <libnidsj@gmail.com>
2025-05-20 15:22:02 -0300

    Update closed_loop_idle.cpp

--- firmware/controllers/math/closed_loop_idle.cpp
+++ firmware/controllers/math/closed_loop_idle.cpp
@@ -81,7 +81,6 @@ void LongTermIdleTrim::update(float rpm, float clt, bool acActive, bool fan1Acti
     }
 
     // Verificar se estamos dentro da faixa de RPM considerada lenta e estável
-	float clt = Sensor::get(SensorType::Clt).value_or(0);
     float rpmDelta = fabsf(rpm - engine->module<IdleController>().unmock().getTargetRpm(clt));
     bool isIdleRpm = rpmDelta < engineConfiguration->ltitStableRpmThreshold;
 
