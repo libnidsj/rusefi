405137b49a8d35b86007c0b1eef21d71ff931c1c
Líbni Junior <libnidsj@gmail.com>
2025-05-26 16:00:40 -0300

    Revert "Update LocalIniFileProvider.java"
    This reverts commit ef1e905594f9b864c6acc6d9a8dcba8bbfa66561.

--- java_console/io/src/main/java/com/rusefi/LocalIniFileProvider.java
+++ java_console/io/src/main/java/com/rusefi/LocalIniFileProvider.java
@@ -3,7 +3,7 @@ package com.rusefi;
 import java.util.Optional;
 
 public class LocalIniFileProvider {
-    public static final String INI_FILE_FOR_SIMULATOR_ROOT_PATH = Optional.ofNullable(System.getenv("SIMULATOR_INI_FILE")).orElse("firmware/tunerstudio/generated/evotech_stm32f767_nucleo.ini");
+    public static final String INI_FILE_FOR_SIMULATOR_ROOT_PATH = Optional.ofNullable(System.getenv("SIMULATOR_INI_FILE")).orElse("firmware/tunerstudio/generated/rusefi_stm32f767_nucleo.ini");
     // f407-discovery is historically the most inclusive .ini file
     public static final String INI_FILE_FOR_SIMULATOR = "../" + INI_FILE_FOR_SIMULATOR_ROOT_PATH;
 }
