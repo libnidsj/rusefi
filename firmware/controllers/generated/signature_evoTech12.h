d941cd48ff0fb234d411d3702f1ec2c71145702b
Líbni Junior <libnidsj@gmail.com>
2025-05-27 14:43:55 -0300

    Wall wetting cleanup

--- firmware/controllers/generated/signature_evoTech12.h
+++ firmware/controllers/generated/signature_evoTech12.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 566441989
-#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12.566441989"
+#define SIGNATURE_HASH 807312862
+#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12.807312862"
--- firmware/controllers/generated/signature_evoTech12plus.h
+++ firmware/controllers/generated/signature_evoTech12plus.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3584659394
-#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12plus.3584659394"
+#define SIGNATURE_HASH 3295947289
+#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12plus.3295947289"
