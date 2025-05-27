176bcb5ec351fce794c06dbbbf17c4060109c3f6
Líbni Junior <libnidsj@gmail.com>
2025-05-26 23:13:02 -0300

    signature_prefix

--- firmware/controllers/generated/signature_evoTech12.h
+++ firmware/controllers/generated/signature_evoTech12.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1392951960
-#define TS_SIGNATURE "evoTech MotroLink.2025.05.27.evoTech12.1392951960"
+#define SIGNATURE_HASH 566441989
+#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12.566441989"
--- firmware/controllers/generated/signature_evoTech12plus.h
+++ firmware/controllers/generated/signature_evoTech12plus.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3976483982
-#define TS_SIGNATURE "evoTech MotroLink.2025.05.27.evoTech12plus.3976483982"
+#define SIGNATURE_HASH 3584659394
+#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12plus.3584659394"
