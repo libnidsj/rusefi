9f4feab54bf287a9e34aea4bc84dc05bdc4b3275
Líbni Junior <libnidsj@gmail.com>
2025-05-26 22:51:29 -0300

    refawctor

--- firmware/controllers/generated/signature_evotech12.h
+++ firmware/controllers/generated/signature_evotech12.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 239185943
-#define TS_SIGNATURE "evoTech motrolink_release.2025.05.27.evotech12.239185943"
+#define SIGNATURE_HASH 1392951960
+#define TS_SIGNATURE "evoTech MotroLink.2025.05.27.evoTech12.1392951960"
